4a6a075bb2d57d6890630ebaa8bbd0c664901369
Mr. Fluffy <git@sixearsm>
2022-11-18 08:03:52 +0000

    add flake; use ghc-8.10.7 for tfmt

--- flake.nix
+++ flake.nix
@@ -2,20 +2,36 @@
   description = "An Exited DataType, to represent program exit better than ()";
 
   inputs = {
-    nixpkgs.url       = "github:nixos/nixpkgs/be44bf67"; # nixos-22.05 2022-10-15
-    build-utils.url   = "github:sixears/flake-build-utils/r1.0.0.11";
+    nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    build-utils.url   = github:sixears/flake-build-utils/r1.0.0.11;
 
-    has-callstack.url = "github:sixears/has-callstack/r1.0.1.12";
-    monaderror-io.url = "github:sixears/monaderror-io/r1.2.5.13";
-    more-unicode.url  = "github:sixears/more-unicode/r0.0.17.8";
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.12;
+    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.13;
+    more-unicode.url  = github:sixears/more-unicode/r0.0.17.8;
   };
 
   outputs = { self, nixpkgs, build-utils
             , has-callstack, monaderror-io, more-unicode }:
     build-utils.lib.hOutputs self nixpkgs "exited" {
-      deps = {
-        inherit has-callstack monaderror-io more-unicode;
-      };
+#      deps = { inherit has-callstack monaderror-io more-unicode; };
       ghc = p: p.ghc8107; # for tfmt
+
+      callPackage = { mkDerivation, lib, system
+                    , base, base-unicode-symbols, data-textual, mtl }:
+        let
+          pkg = build-utils.lib.flake-def-pkg system;
+        in
+          mkDerivation {
+            pname = "exited";
+            version = "1.0.4.16";
+            src = ./.;
+            libraryHaskellDepends = [
+              base base-unicode-symbols data-textual mtl
+            ] ++ map (p: pkg p) [ has-callstack monaderror-io more-unicode ];
+
+            testHaskellDepends = [ base ];
+            description = "An Exited DataType, to represent program exit better than ()";
+            license = lib.licenses.mit;
+          };
     };
 }
