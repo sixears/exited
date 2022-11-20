c3a576afb40ef6af984d9ac6a8b0815eb6a12bf7
Mr. Fluffy <git@sixearsm>
2022-11-20 07:17:39 +0000

    has-callstack->1.0.1.19

--- flake.nix
+++ flake.nix
@@ -5,8 +5,8 @@
     nixpkgs.url       = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
     build-utils.url   = github:sixears/flake-build-utils/r1.0.0.13;
 
-    has-callstack.url = github:sixears/has-callstack/r1.0.1.18;
-    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.19;
+    has-callstack.url = github:sixears/has-callstack/r1.0.1.19;
+    monaderror-io.url = github:sixears/monaderror-io/r1.2.5.20;
     more-unicode.url  = github:sixears/more-unicode/r0.0.17.12;
   };
 
@@ -23,7 +23,7 @@
         in
           mkDerivation {
             pname = "exited";
-            version = "1.0.4.22";
+            version = "1.0.4.23";
             src = ./.;
             libraryHaskellDepends = [
               base base-unicode-symbols data-textual mtl
